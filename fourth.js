81619508bbc8286438f9bafe9d7a68507a9f4c50
Pete Benoit <peterbenoit@gmail.com>
2021-11-10 12:24:46 -0600

    Update fourth.js

--- fourth.js
+++ fourth.js
@@ -17,4 +17,11 @@ link.rel = "stylesheet";
 link.href = 'https://www.cdc.gov/wcms/4.0/cdc-wp/data-presentation/examples/headings-css/heading-styles-4.css';
 link.title = 'fontstylesheet';
 
+var font = document.createElement( 'link' );
+font.type = "text/css";
+font.rel = "stylesheet";
+font.href = 'https://fonts.googleapis.com/css2?family=Open+Sans:wght@500;600&display=swap';
+font.title = 'fontstylesheet';
+
 head.appendChild( link );
+head.appendChild( font );
